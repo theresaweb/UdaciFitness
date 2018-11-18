49512888fc8e24879560efe5d68ba4b524b8da68
Theresa Newman <theresa.webdev@gmail.com>
2018-11-18 17:46:43 -0500

    add UdaciSteppers UI add already recorded today and reset screen debug issue with Ionicon missing

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import { Ionicons } from '@expo/vector-icons';
+import TextButton from './TextButton'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -55,8 +57,28 @@ submit = () => {
    // Save to "DB"
    // Clear local notification
 }
+reset = () => {
+  const key = timeToString()
+   // Update Redux
+   // Route to Home
+   // Update "DB"
+}
 render() {
   const metaInfo = getMetricMetaInfo()
+  if (this.props.alreadyLogged) {
+    return (
+      <View>
+      <Ionicons
+         name={'md-happy'}
+        size={100}
+      />
+        <Text>You already logged your information for today.</Text>
+        <TextButton onPress={this.reset}>
+          Reset
+        </TextButton>
+      </View>
+    )
+  }
   return (
     <View>
     <DateHeader date={(new Date()).toLocaleDateString()}/>
