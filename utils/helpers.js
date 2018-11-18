661a99740531dfd01328d31bd80c1ca2a579ab6f
Theresa Newman <theresa.webdev@gmail.com>
2018-11-18 13:41:06 -0500

    add AddEntry component and test with getMetricMetaInfo function

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,107 @@
-export function isBetween (num, x, y) {
+import React from 'react'
+import { View } from 'react-native'
+import { FontAwesome, MaterialIcons, MaterialCommunityIcons } from '@expo/vector-icons'
+import { white, black } from './colors'
+
+export function getMetricMetaInfo (metric) {
+  const info = {
+    run: {
+      displayName: 'Run',
+      max: 50,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialIcons
+              name='directions-run'
+              color={white}
+              size={35}
+            />
+          </View>
+        )
+      }
+    },
+    bike: {
+      displayName: 'Bike',
+      max: 100,
+      unit: 'miles',
+      step: 1,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons
+              name='bike'
+              color={black}
+              size={32}
+            />
+          </View>
+        )
+      }
+    },
+    swim: {
+      displayName: 'Swim',
+      max: 9900,
+      unit: 'meters',
+      step: 100,
+      type: 'steppers',
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons
+              name='swim'
+              color={white}
+              size={35}
+            />
+          </View>
+        )
+      }
+    },
+    sleep: {
+      displayName: 'Sleep',
+      max: 24,
+      unit: 'hours',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+          <View>
+            <FontAwesome
+              name='bed'
+              color={white}
+              size={30}
+            />
+          </View>
+        )
+      }
+    },
+    eat: {
+      displayName: 'Eat',
+      max: 10,
+      unit: 'rating',
+      step: 1,
+      type: 'slider',
+      getIcon() {
+        return (
+          <View>
+            <MaterialCommunityIcons
+              name='food'
+              color={white}
+              size={35}
+            />
+          </View>
+        )
+      }
+    },
+  }
+   return typeof metric === 'undefined'
+    ? info
+    : info[metric]
+}
+
+export function isBetween(num, x, y) {
   if (num >= x && num <= y) {
     return true
   }
@@ -6,7 +109,7 @@ export function isBetween (num, x, y) {
   return false
 }
 
-export function calculateDirection (heading) {
+export function calculateDirection(heading) {
   let direction = ''
 
   if (isBetween(heading, 0, 22.5)) {
@@ -34,7 +137,7 @@ export function calculateDirection (heading) {
   return direction
 }
 
-export function timeToString (time = Date.now()) {
+export function timeToString(time = Date.now()) {
   const date = new Date(time)
   const todayUTC = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
   return todayUTC.toISOString().split('T')[0]
