e9d061d719ccabdca02e665f3545dc35f4ff33d9
Theresa Newman <theresa.webdev@gmail.com>
2018-11-19 14:50:06 -0500

    add udacifitness-calendar module and implement in History.js

--- components/History.js
+++ components/History.js
@@ -4,7 +4,9 @@ import { connect } from 'react-redux'
 import { receiveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
- class History extends Component {
+import UdaciFitnessCalendar from 'udacifitness-calendar'
+
+class History extends Component {
   componentDidMount () {
     const { dispatch } = this.props
      fetchCalendarResults()
@@ -18,13 +20,30 @@ import { fetchCalendarResults } from '../utils/api'
       })
       .then(() => this.setState(() => ({ready: true})))
   }
-  render() {
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View>
+      {today
+        ? <Text>{JSON.stringify(today)}</Text>
+        : <Text>{JSON.stringify(metrics)}</Text>}
+    </View>
+  )
+  renderEmptyDate(formattedDate) {
     return (
       <View>
-        <Text>{JSON.stringify(this.props)}</Text>
+        <Text>No Data for this day</Text>
       </View>
     )
   }
+render() {
+  const { entries } = this.props
+   return (
+    <UdaciFitnessCalendar
+      items={entries}
+      renderItem={this.renderItem}
+      renderEmptyDate={this.renderEmptyDate}
+    />
+  )
+}
 }
  function mapStateToProps (entries) {
   return {
