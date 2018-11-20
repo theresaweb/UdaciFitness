08612a5aa101ee5b7efabef07692137b5eddec6c
Theresa Newman <theresa.webdev@gmail.com>
2018-11-20 14:24:45 -0500

    add animation to live view

--- components/Live.js
+++ components/Live.js
@@ -1,22 +1,67 @@
 import React, { Component } from 'react'
-import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet, Animated } from 'react-native'
 import { Foundation } from '@expo/vector-icons'
 import { purple, white } from '../utils/colors'
+import {Location,Permissions} from 'expo'
+import {calculateDirection} from '../utils/helpers'
 
  export default class Live extends Component {
   state = {
     coords: null,
-    status: 'granted',
-    direction: ''
+    status: null,
+    direction: '',
+    bounceValue: new Animated.Value(1),
+  }
+  componentDidMount () {
+    Permissions.getAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
+         this.setState(() => ({ status }))
+      })
+      .catch((error) => {
+        console.warn('Error getting Location permission: ', error)
+         this.setState(() => ({ status: 'undetermined' }))
+      })
   }
   askPermission = () => {
+    Permissions.askAsync(Permissions.LOCATION)
+      .then(({ status }) => {
+        if (status === 'granted') {
+          return this.setLocation()
+        }
+         this.setState(() => ({ status }))
+      })
+      .catch((error) => console.warn('error asking Location permission: ', error))
+  }
+  setLocation = () => {
+    Location.watchPositionAsync({
+      enableHighAccuracy: true,
+      timeInterval: 1,
+      distanceInterval: 1,
+    }, ({ coords }) => {
+      const newDirection = calculateDirection(coords.heading)
+      const { direction, bounceValue } = this.state
+      if (newDirection !== direction) {
+        Animated.sequence([
+          Animated.timing(bounceValue, { duration: 200, toValue: 1.04}),
+          Animated.spring(bounceValue, { toValue: 1, friction: 4})
+        ]).start()
+      }
+      this.setState(() => ({
+        coords,
+        status: 'granted',
+        direction: newDirection,
+      }))
+    })
   }
   render() {
-    const { status, coords, direction } = this.state
-     if (status === null) {
+    const { status, coords, direction, bounceValue } = this.state
+    if (status === null) {
       return <ActivityIndicator style={{marginTop: 30}}/>
     }
-     if (status === 'denied') {
+     if (status === 'undetermined') {
       return (
         <View style={styles.center}>
           <Foundation name='alert' size={50} />
@@ -26,7 +71,7 @@ import { purple, white } from '../utils/colors'
         </View>
       )
     }
-     if (status === 'undetermined') {
+     if (status === 'denied') {
       return (
         <View style={styles.center}>
           <Foundation name='alert' size={50} />
@@ -43,13 +88,12 @@ import { purple, white } from '../utils/colors'
     }
      return (
       <View style={styles.container}>
-        <Text>Live</Text>
-        <Text>{JSON.stringify(this.state)}</Text>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>
-            North
-          </Text>
+          <Animated.Text
+            style={[styles.direction, {transform: [{scale: bounceValue}]}]}>
+              {direction}
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
@@ -57,7 +101,7 @@ import { purple, white } from '../utils/colors'
               Altitude
             </Text>
             <Text style={[styles.subHeader, {color: white}]}>
-              {200} feet
+              {Math.round(coords.altitude * 3.2808)} Feet
             </Text>
           </View>
           <View style={styles.metric}>
@@ -65,7 +109,7 @@ import { purple, white } from '../utils/colors'
               Speed
             </Text>
             <Text style={[styles.subHeader, {color: white}]}>
-              {300} MPH
+              {(coords.speed * 2.2369).toFixed(1)} MPH
             </Text>
           </View>
         </View>
