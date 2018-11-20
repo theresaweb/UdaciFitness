625471b673455f55ff0a139d5b673b61944d3dd0
Theresa Newman <theresa.webdev@gmail.com>
2018-11-20 13:18:11 -0500

    add styling and content to denied permission view

--- components/Live.js
+++ components/Live.js
@@ -6,7 +6,7 @@ import { purple, white } from '../utils/colors'
  export default class Live extends Component {
   state = {
     coords: null,
-    status: 'undetermined',
+    status: 'denied',
     direction: ''
   }
   askPermission = () => {
@@ -18,8 +18,11 @@ import { purple, white } from '../utils/colors'
     }
      if (status === 'denied') {
       return (
-        <View>
-          <Text>Denied</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50} />
+          <Text>
+            You denied your location. You can fix this by visiting your settings and enabling location services for this app.
+          </Text>
         </View>
       )
     }
