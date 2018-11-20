7d511b01d5e34add3504711dd32271296f6988e8
Theresa Newman <theresa.webdev@gmail.com>
2018-11-20 11:20:51 -0500

    add navigation back to home for addentry and reset

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -14,6 +14,7 @@ import { submitEntry, removeEntry } from '../utils/api'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { purple, white } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -64,7 +65,7 @@ submit = () => {
     [key]: entry
   }))
    this.setState(() => ({ run: 0, bike: 0, swim: 0, sleep: 0, eat: 0 }))
-   // Navigate to home
+   this.toHome()
    submitEntry({ key, entry })
    // Clear local notification
 }
@@ -73,8 +74,11 @@ reset = () => {
   this.props.dispatch(addEntry({
     [key]: getDailyReminderValue()
   }))
-   // Route to Home
+   this.toHome()
    removeEntry(key)
+}
+toHome = () => {
+  this.props.navigation.dispatch(NavigationActions.back({key: 'AddEntry'}))
 }
 render() {
   const metaInfo = getMetricMetaInfo()
